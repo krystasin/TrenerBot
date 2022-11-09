ad5ace3a82ccac188a129092ff1c6ff404f99c52
Krystasin <krystyn.brzyski@gmail.com>
2022-11-09 20:15:05 +0100

    init

---
+++ adapter.py
@@ -0,0 +1,60 @@
+from chatterbot.logic import LogicAdapter
+import re
+
+class BmiResponseAdapter(LogicAdapter):
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+        self.waga = 0
+        self.wzrost = 0
+
+    def can_process(self, statement):
+        wzrost_reg2 = re.compile(r'((mam)\s*(\d+[,\.]?\d*))|(\d+[,\.]?\d*)\s*(wzrostu|wysokości|cm|m)', re.I)
+        waga_reg = re.compile(r'((ważę|waże)\s+\d+)|(\d+\s*kg)', re.I)
+
+        text = statement.text
+        wzrost = wzrost_reg2.search(text)
+        waga = waga_reg.search(text)
+
+        if re.search(r'bmi', text, re.I):
+            return True
+
+        if waga != None:
+            waga_str = re.search(r'\d+', waga.group())
+            self.waga = int(waga_str[0])
+
+        if wzrost != None:
+            wzrost_str = re.search(r'\d+[,\.]?\d*', wzrost.group()).group()
+            wzrost_str = wzrost_str.replace(',', '.')
+            self.wzrost = float(wzrost_str)
+
+        if waga != None or wzrost != None:
+            print('waga: {}, wzrost: {}'.format(self.waga, self.wzrost))
+            return True
+        else:
+            return False
+
+    def process(self, input_statement, additional_response_selection_parameters):
+        from chatterbot.conversation import Statement
+
+        if self.waga == 0:
+            return Statement(text='ile ważysz ?')
+        if self.wzrost == 0:
+            return Statement(text='ile masz wzrostu ?')
+        if self.wzrost >10:
+            self.wzrost /= 100
+
+        bmi = self.waga / (self.wzrost * self.wzrost)
+        bmi = bmi.__round__(2)
+
+        if bmi < 18.5:
+            rezultat = 'Musisz jeść więcej'
+        elif bmi <25:
+            rezultat = 'Jesteś w normie'
+        elif bmi <23:
+            rezultat = 'Przydało by się schudnąć'
+        elif bmi <35:
+            rezultat = 'Łooo panie, marsz na siłke'
+        else:
+            rezultat = 'RIP twoje serducho'
+
+        return Statement(text='{}. Twoje bmi wynosi {}'.format(rezultat, bmi ))
---
+++ main.py
@@ -0,0 +1,115 @@
+from chatterbot import ChatBot
+from chatterbot.conversation import Statement
+from chatterbot.trainers import ListTrainer
+import re
+
+waga_reg = r'%d+\s*kg'
+inp = '44 kg'
+res = re.findall(waga_reg, inp )
+
+
+
+
+
+
+
+
+
+
+
+
+chatbot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'adapter.BmiResponseAdapter',
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.90
+        },
+        "chatterbot.logic.BestMatch"
+    ]
+)
+listTrainer=ListTrainer(chatbot)
+
+listTrainer.train([
+    "Hej",
+    "Witaj",
+    "Można zapisać się na trening ?",
+    "Oczywiście",
+])
+listTrainer.train([
+    "Cześć",
+    "Idealny dzień na trening, co nie?",
+    "No nup"
+    "ie wiem, troche mi się nie chce.",
+    "Z takim nastawieniem zawsze będziesz gruby.",
+    "To co mam zrobić aby to zmienić ?",
+    "Nie pytaj się tylko to zrób.",
+    "Ale co ?",
+    "Masz tutaj plan, na rozgrzewke zrób 10 serii po 20"
+])
+listTrainer.train([
+    "co mam teraz zrobić",
+    "brzuszki",
+    "a teraz",
+    "pompeczki"
+])
+listTrainer.train([
+    "czy masz dla mnie jakiś plan",
+    "mam mnóstwo, ale nie wiem czy ci sie one spodobają",
+    "co przygotowałeś",
+    "ta lista cię zwali z nóg"
+])
+listTrainer.train([
+    "idziesz na trening",
+    "nie ide",
+    "to nie było pytanie"
+])
+listTrainer.train([
+    "co dziś robimy",
+    "ty będziesz się pocić, a ja zarabiać hajs",
+    "troche to nie fair",
+    "życie jest nie fair, a teraz wskakuj na bieżnie",
+    "za niedługo stracisz klienta",
+])
+listTrainer.train([
+    "Gdzie jest łazienka ?",
+    "Paradoksalnie to głównie dieta.",
+    "A toaleta gdzie jest ?",
+    "sześć razy w tygodniu po trzy godziny"
+])
+listTrainer.train([
+    "jaką dietę polecasz ?",
+    "dużo mięsa, dużo warzyw. Dużo wszystkiego !",
+    "zero igieł, wszystko naturalnegdzie "
+])
+listTrainer.train([
+    "wiesz jaki jest czas",
+    "czas na trening gościu",
+    "ale jaka godzina",
+    "nie gadaj tyle tylko ćwicz"
+])
+listTrainer.train([
+    "co teraz mam robić ?",
+    "50 pompek",
+    "a potem co zrobić",
+    "poprzeglądać się w lustrze tak z 10min"
+])
+listTrainer.train([
+    "100",
+    "ale czego ?!",
+    "120",
+    "czego, sekund ?!",
+])
+
+
+
+
+while True:
+    user_input = input(">")
+    if "nara" in user_input.lower():
+        print("Trzymaj się, widzimy się jutro o tej samej porze")
+        break
+    response=chatbot.get_response(user_input)
+    print(response)
